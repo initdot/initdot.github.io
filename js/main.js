7f3a8a22dec24645e2694dfe376a1fedcbc2b786
palerdot <palerdot@gmail.com>
2015-02-06 18:15:43 +0530

    [SITE UPDATE] minor layout changes for initdot site; Less used

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+( function ($) {
 
+    $(window).on("scroll", function () {
+    
+        var isScrolled = true;
+        
+        $(".header").css({
+            "box-shadow": "0 0 0.75em #dedede"
+        });
+        
+        // remove the header animation 1 sec after scrolling
+        setTimeout( function(){
+        
+            isScrolled = false;
+            
+            if (!isScrolled) {
+                $(".header").css({
+                    "box-shadow": "0 0 0.25em #dedede"
+                });
+            }
+        
+        }, 1000 );
+        
+    });
+
+} (jQuery) );
